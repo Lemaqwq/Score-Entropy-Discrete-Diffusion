0bcf9af557a1f69fd6de25228cbd20b2ae6a32b5
Lemaqwq <clh648@connect.hku.hk>
2025-02-27 23:53:24 -0800

    update run sample script

--- run_sample_cond.sh
+++ run_sample_cond.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-steps_list=(64)
+steps_list=(4 8 16 32 64)
 for steps in "${steps_list[@]}"; do
     CUDA_VISIBLE_DEVICES=0 python gsm8k_run_sample_cond.py \
-        --model_path exp_local/gsm8k/2024.05.15/215827_medium_step_60000 \
+        --model_path exp_local/gsm8k/2025.02.27/094449 \
         --dataset gsm8k \
         --steps $steps \
         --batch_size 1
     echo "steps: $steps done"
 done
-
