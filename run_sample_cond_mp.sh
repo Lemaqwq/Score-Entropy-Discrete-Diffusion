2226636e862e6a89bb5accf605283e4b9af37014
Lemaqwq <clh648@connect.hku.hk>
2025-03-01 09:10:26 -0800

    finalize the code draft

--- run_sample_cond_mp.sh
+++ run_sample_cond_mp.sh
@@ -1,10 +1,10 @@
-steps_list=(64)
+steps_list=(4 8 16 32 64)
 for steps in "${steps_list[@]}"; do
-    CUDA_VISIBLE_DEVICES=0 python gsm8k_run_sample_cond_mp_1.py \
+    CUDA_VISIBLE_DEVICES=0 python gsm8k_run_sample_cond_mp.py \
         --model_path exp_local/gsm8k/2025.03.01/013931_dot_mp_small_60000 \
         --dataset gsm8k \
         --steps $steps \
-        --batch_size 1
+        --batch_size 16
     echo "steps: $steps done"
 done
 
