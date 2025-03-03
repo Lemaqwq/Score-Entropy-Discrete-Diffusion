7692e1827fab5d724f8fe37bbf7be6b9dd0dfedb
Lemaqwq <clh648@connect.hku.hk>
2025-03-02 20:35:15 -0800

    reformat inference dot

--- gsm8k_run_sample_cond.py
+++ gsm8k_run_sample_cond.py
@@ -31,7 +31,6 @@ def main():
 
     tokenizer = get_tokenizer()
 
-
     test_set = finetune_get_dataset(args.dataset, "test", tokenizer=tokenizer, multipass=False, hidden_thought=True)
 
     test_loader = DataLoader(
@@ -48,23 +47,20 @@ def main():
 
     test_iter = iter(test_loader)
 
+    model_name = args.model_path.split("/")[-1]
+    output_dir = f"generated_output/{args.dataset}/{model_name}/"
+    os.makedirs(output_dir, exist_ok=True)
+
     def proj_fun(x):
         x = torch.where(input_mask==0, input_ids, x)
         return x
-
-
-
-    output_dir = f"generated_output/{args.dataset}/dot_medium"
-    os.makedirs(output_dir, exist_ok=True)
-
+    
     run_time = []
     for batch in tqdm(test_iter, desc="Processing batches", unit="batch"):
         start_time = time.time()
         input_ids = batch["input_ids"].to(device)
         input_mask = batch["input_mask"].to(device)
         curr_batch_sz = len(input_ids)
-
-
 
         sampling_fn = sampling.get_dot_pc_sampler(
             graph, noise, (curr_batch_sz, block_size), 'analytic', args.steps, device=device, proj_fun=proj_fun
